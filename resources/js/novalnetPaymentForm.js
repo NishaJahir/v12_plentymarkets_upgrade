0519f0e56213d3777ef467b17267a3ed72cfeff4
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2020-12-08 16:58:19 +0530

    Update novalnetPaymentForm.js

--- resources/js/novalnetPaymentForm.js
+++ resources/js/novalnetPaymentForm.js
@@ -87,12 +87,6 @@ $(document).ready( function () {
         let iban = $(this).val().replace( /[^a-zA-Z0-9]+/g, "" ).replace( /\s+/g, "" );
             $(this).val(iban);      
         });
-    
-        $('#nnSepaCardholder').keypress(function (event) {
-         var keycode = ( 'which' in event ) ? event.which : event.keyCode,
-         reg     = /[^0-9\[\]\/\\#,+@!^()$~%'"=:;<>{}\_\|*?`]/g;
-         return ( reg.test( String.fromCharCode( keycode ) ) || 0 === keycode || 8 === keycode );
-         });
 
         $('#novalnetForm').on('submit',function(){
           $('#novalnetFormBtn').attr('disabled',true);      
