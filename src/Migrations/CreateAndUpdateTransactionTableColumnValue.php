7137cd7c81c8d49b95c123447331514036a86cb9
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2020-12-09 17:43:06 +0530

    Update and rename CreateAndUpdateTransactionTableColumn.php to CreateAndUpdateTransactionTableColumnValue.php

--- src/Migrations/CreateAndUpdateTransactionTableColumnValue.php
+++ src/Migrations/CreateAndUpdateTransactionTableColumnValue.php
@@ -21,7 +21,7 @@ use Plenty\Modules\Plugin\DataBase\Contracts\Migrate;
  * Class UpgradeTransactionTable
  * @package Novalnet\Migrations
  */
-class CreateAndUpdateTransactionTableColumn
+class CreateAndUpdateTransactionTableColumnValue
 {
     /**
      * Create and update transaction log table
