4e093ce224768ecdbf129d2b24ceece498e5e2f0
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2020-12-14 11:54:14 +0530

    Update NovalnetInstalmentbyInvoicePaymentMethod.php

--- src/Methods/NovalnetInstalmentbyInvoicePaymentMethod.php
+++ src/Methods/NovalnetInstalmentbyInvoicePaymentMethod.php
@@ -106,7 +106,7 @@ class NovalnetInstalmentbyInvoicePaymentMethod extends PaymentMethodBaseService
         $paymentConditionValidation = $this->paymentService->checkPaymentDisplayConditions($this->basket, 'novalnet_instalment_invoice');
 
         $this->getLogger(__METHOD__)->error('codn', $paymentConditionValidation);
-        return (bool)($this->paymentHelper->paymentActive() && $active_payment_allowed_country && $active_payment_minimum_amount && $active_payment_maximum_amount);
+        return (bool)($this->paymentHelper->paymentActive() && $active_payment_allowed_country && $active_payment_minimum_amount && $active_payment_maximum_amount & $instalment_payment_mimimum_amount & $paymentConditionValidation);
         }
         return false;
 
