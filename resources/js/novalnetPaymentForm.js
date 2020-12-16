6daf5b9a8be78bcfaad6f27b1e5758529c86614e
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2020-12-16 12:46:22 +0530

    Update novalnetPaymentForm.js

--- resources/js/novalnetPaymentForm.js
+++ resources/js/novalnetPaymentForm.js
@@ -7,34 +7,34 @@ console.log(res);
 $(document).ready( function () {
     
     //~ Save card details process
-        if (jQuery("#nnToggleForm").length <= 0 || jQuery("#nnToggleForm").is(':checked') ) {
-            jQuery("#radioOption").show();
+        if ($("#nnToggleForm").length <= 0 || $("#nnToggleForm").is(':checked') ) {
+            $("#radioOption").show();
         } else {
-            jQuery("#newCardDetails").hide();
+            $("#newCardDetails").hide();
         }
         
-         jQuery("input[type='radio']").click(function(){
-            var tokenValue = jQuery("input[id='maskingDetails']:checked").val();
+         $("input[type='radio']").click(function(){
+            var tokenValue = $("input[id='maskingDetails']:checked").val();
             if(tokenValue){
-                var token = jQuery("#nn_saved_" + payment_name  + "_token").val();
-                jQuery('#nnSelected'+ res[0] + res[1] +'_token').val(token);
+                var token = $("#nn_saved_" + payment_name  + "_token").val();
+                $('#' +  res[0] + res[1] + 'nnSelectedToken').val(token);
             } else {
-                jQuery('#nnSelected'+ res[0] + res[1] +'_token').val('');
+                $('#' +  res[0] + res[1] + 'nnSelectedToken').val('');
             }
     });
 
-        jQuery("input[name='radioOption']").on(
+        $("input[name='radioOption']").on(
             'click', function () {
-                if(jQuery(this).attr('id') == 'nnToggleForm') {
-                    jQuery("#newCardDetails").show();
-                    jQuery("#newForm").val('1');
+                if($(this).attr('id') == 'nnToggleForm') {
+                    $("#newCardDetails").show();
+                    $("#newForm").val('1');
                 } else {
-                    jQuery("#newCardDetails").hide();
+                    $("#newCardDetails").hide();
                 }
             }
         );
     
-        jQuery("input[name='radioOption']:first").attr("checked","checked");
+        $("input[name='radioOption']:first").attr("checked","checked");
     
     // For credit card payment form process
         if (paymentName == 'NOVALNET_CC') {
