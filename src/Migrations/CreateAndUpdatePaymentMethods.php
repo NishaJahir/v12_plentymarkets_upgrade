fa626ae90a28c2d701f2296e01d145c31e01ac98
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2020-12-26 18:44:00 +0530

    Update CreateAndUpdatePaymentMethods.php

--- src/Migrations/CreateAndUpdatePaymentMethods.php
+++ src/Migrations/CreateAndUpdatePaymentMethods.php
@@ -71,7 +71,7 @@ class CreateAndUpdatePaymentMethods
         $payment_data = $this->paymentHelper->getPaymentMethodByKey($paymentKey);
         if ($payment_data == 'no_paymentmethod_found')
         {
-            $paymentMethodData = ['pluginKey'  => 'Novalnet',
+            $paymentMethodData = ['pluginKey'  => 'plenty_novalnet',
                                 'paymentKey' => $paymentKey,
                                 'paymentName' => $paymentName
                                ];
