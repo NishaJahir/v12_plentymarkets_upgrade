4c2260a458a8ed35bbb2e4f0c282202a258f8921
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2020-12-26 12:34:02 +0530

    Update NovalnetOrderConfirmationDataProvider.php

--- src/Providers/NovalnetOrderConfirmationDataProvider.php
+++ src/Providers/NovalnetOrderConfirmationDataProvider.php
@@ -15,21 +15,12 @@
 
 namespace Novalnet\Providers;
 
-use Plenty\Plugin\Templates\Twig;
-
-use Novalnet\Helper\PaymentHelper;
-use Plenty\Modules\Order\Models\Order;
-use Plenty\Modules\Payment\Models\Payment;
-use Plenty\Modules\Comment\Contracts\CommentRepositoryContract;
-use Plenty\Modules\Payment\Contracts\PaymentRepositoryContract;
 use \Plenty\Modules\Authorization\Services\AuthHelper;
-use Plenty\Modules\Frontend\Session\Storage\Contracts\FrontendSessionStorageFactoryContract;
 use Novalnet\Services\PaymentService;
-use Novalnet\Services\TransactionService;
+use Plenty\Plugin\Templates\Twig;
 
 /**
  * Class NovalnetOrderConfirmationDataProvider
- *
  * @package Novalnet\Providers
  */
 class NovalnetOrderConfirmationDataProvider
@@ -37,59 +28,16 @@ class NovalnetOrderConfirmationDataProvider
     /**
      * Setup the Novalnet transaction comments for the requested order
      *
-     * @param Twig $twig
-     * @param PaymentRepositoryContract $paymentRepositoryContract
      * @param Arguments $arg
-     * @return string
      */
-    public function call(Twig $twig, PaymentRepositoryContract $paymentRepositoryContract, $arg)
+    public function call(Twig $twig, $arg)
     {
-        $paymentHelper = pluginApp(PaymentHelper::class);
         $paymentService = pluginApp(PaymentService::class);
-        $transactionLog  = pluginApp(TransactionService::class); 
-        $sessionStorage = pluginApp(FrontendSessionStorageFactoryContract::class);
         $order = $arg[0];
         if (!empty ($order['id'])) {
-			$payments = $paymentRepositoryContract->getPaymentsByOrderId($order['id']);
-		$paymentHelper->logger('payment conf', $payments);
-            foreach($payments as $payment)
-            {
-                $properties = $payment->properties;
-                foreach($properties as $property)
-                {
-                    if ($property->typeId == 30)
-                    {
-                    $tidStatus = $property->value;
-                    }
-                }
-                if($paymentHelper->getPaymentKeyByMop($payment->mopId))
-                {
-                    $orderId = (int) $payment->order['orderId'];
-                    $comment = '';
-                    $paymentDetails = $paymentService->getDatabaseValues($orderId);
-                    $paymentHelper->logger('db1', $paymentDetails);
-                    $comments = '';
-                    $comments .= PHP_EOL . $paymentHelper->getTranslatedText('nn_tid') . $paymentDetails['tid'];
-                    if(!empty($paymentDetails['test_mode'])) {
-                        $comments .= PHP_EOL . $paymentHelper->getTranslatedText('test_order');
-                    }
-                    $getTransactionDetails = $transactionLog->getTransactionData('orderNo', $orderId);
-                    $totalCallbackAmount = 0;
-                    foreach ($getTransactionDetails as $transactionDetail) {
-                       $totalCallbackAmount += $transactionDetail->callbackAmount;
-                    }
-                    
-                    if(in_array($tidStatus, ['PENDING', 'ON_HOLD', 'SUCCESS']) && ($paymentDetails['invoice_type'] == 'INVOICE' && ($transactionDetail->amount > $totalCallbackAmount) || $paymentDetails['payment_id'] == 96) ) {
-                        //$bankDetails .= PHP_EOL . $paymentService->getInvoicePrepaymentComments($db_details);
-                        $paymentHelper->logger('invoice comments called', $transactionDetail);
-			    $bankDetails = $paymentDetails;
-                    }
-                }
-            }
-                    $comment .= (string) $comments;
-                    $comment .= PHP_EOL;
-        }   
-                  return $twig->render('Novalnet::NovalnetOrderHistory', ['bankDetails' => $bankDetails, 'paymentDetails' => $paymentDetails]);
+            $transactionCommentVal = $paymentService->getTransactionCommentVal($order['id']);
+            return $twig->render('Novalnet::NovalnetOrderHistory', ['bankDetails' => $transactionCommentVal['bankDetails'], 'transactionDetails' => $transactionCommentVal['transactionDetails']]);
+        }
     }
 }
 
