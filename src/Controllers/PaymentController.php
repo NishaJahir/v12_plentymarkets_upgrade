96ced476a4b61176b88f1abf9bf7045be35f285d
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2020-12-26 12:31:02 +0530

    Update PaymentController.php

--- src/Controllers/PaymentController.php
+++ src/Controllers/PaymentController.php
@@ -23,17 +23,15 @@ use Novalnet\Helper\PaymentHelper;
 use Plenty\Modules\Basket\Contracts\BasketRepositoryContract;
 use Novalnet\Services\PaymentService;
 use Plenty\Plugin\Templates\Twig;
+use Novalnet\Constants\NovalnetConstants;
 use Plenty\Plugin\ConfigRepository;
-use Plenty\Plugin\Log\Loggable;
 use Novalnet\Services\TransactionService;
 /**
  * Class PaymentController
- *
  * @package Novalnet\Controllers
  */
 class PaymentController extends Controller
 {
-    use Loggable;
     /**
      * @var Request
      */
@@ -74,12 +72,11 @@ class PaymentController extends Controller
      */
     private $config;
 
-	
-	/**
+    /**
      * @var transaction
      */
     private $transaction;
-	
+    
     /**
      * PaymentController constructor.
      *
@@ -90,6 +87,7 @@ class PaymentController extends Controller
      * @param SessionStorageService $sessionStorage
      * @param BasketRepositoryContract $basketRepository
      * @param PaymentService $paymentService
+     * @param TransactionService $tranactionService,
      * @param Twig $twig
      */
     public function __construct(  Request $request,
@@ -99,7 +97,7 @@ class PaymentController extends Controller
                                   FrontendSessionStorageFactoryContract $sessionStorage,
                                   BasketRepositoryContract $basketRepository,             
                                   PaymentService $paymentService,
-				 TransactionService $tranactionService,
+                                  TransactionService $tranactionService,
                                   Twig $twig
                                 )
     {
@@ -111,138 +109,101 @@ class PaymentController extends Controller
         $this->basketRepository  = $basketRepository;
         $this->paymentService  = $paymentService;
         $this->twig            = $twig;
-	    $this->transaction = $tranactionService;
+        $this->transaction     = $tranactionService;
         $this->config          = $config;
     }
 
     /**
-     * Novalnet redirects to this page if the payment was executed successfully
+     * Handled the redirect payments after initial payment call success
      *
      */
-    public function paymentResponse() {
+    public function paymentResponse() 
+    {
         $requestData = $this->request->all();
-	    $this->getLogger(__METHOD__)->error('payment response', $requestData);
-	   
-	  $responseData = $this->checksumForRedirects($requestData);
-	    $this->getLogger(__METHOD__)->error('payment response333', $responseData);
-
-				
-	
+        $responseData = $this->checksumForRedirects($requestData);
         $isPaymentSuccess = isset($responseData['result']['status']) && in_array($responseData['result']['status'], ['PENDING', 'SUCCESS']);
-        $notificationMessage = $this->paymentHelper->getTranslatedText('payment_success');
+        $notificationMessage = $this->paymentHelper->getTranslatedText('paymentSuccess');
         if ($isPaymentSuccess) {
             $this->paymentService->pushNotification($notificationMessage, 'success', 100);
         } else {
-            $this->paymentService->pushNotification($notificationMessage, 'error', 100);    
+            $this->paymentService->pushNotification($responseData['status_text'], 'error', 100);    
         }
-        
-        //$responseData['test_mode'] = $this->paymentHelper->decodeData($responseData['test_mode'], $responseData['uniqid']);
-       // $responseData['amount']    = $this->paymentHelper->decodeData($responseData['amount'], $responseData['uniqid']) / 100;
-        $paymentRequestData = $this->sessionStorage->getPlugin()->getValue('nnPaymentData');
-        $this->sessionStorage->getPlugin()->setValue('nnPaymentData', array_merge($paymentRequestData, $responseData));
-        $this->paymentService->validateResponse();
+        $paymentRequestParameters = $this->sessionStorage->getPlugin()->getValue('nnPaymentData');
+        $this->sessionStorage->getPlugin()->setValue('nnPaymentData', array_merge($paymentRequestParameters, $responseData));
+        $this->paymentService->validatePaymentResponse();
         return $this->response->redirectTo('confirmation');
     }
-	
-	public function checksumForRedirects($response)
+    
+    /**
+     * Checksum validation for redirect payment
+     *
+     */
+    public function checksumForRedirects($response) 
     {
-		$this->getLogger(__METHOD__)->error('checksum', $response);
-		
-		$strRevKey = implode(array_reverse(str_split($this->paymentHelper->getNovalnetConfig('novalnet_access_key'))));
+        $strRevKey = implode(array_reverse(str_split(trim($this->config->get('Novalnet.novalnet_access_key')))));
         // Condition to check whether the payment is redirect
         if (! empty($response['checksum']) && ! empty($response['tid']) && !empty($response['txn_secret']) && !empty($response['status'])) {
             $token_string = $response['tid'] . $response['txn_secret'] . $response['status'] . $strRevKey;
             $generated_checksum = hash('sha256', $token_string);
             if ($generated_checksum !== $response['checksum']) {
-		    $notificationMessage = 'Checksum is invalid';
-                $this->paymentService->pushNotification($notificationMessage, 'error', 100);
-		 return $this->response->redirectTo('checkout');
-            } else {
-            $data = [];
-            $data['transaction']['tid'] = $response['tid'];
-           
-            $responseData = $this->paymentHelper->executeCurl(json_encode($data), 'https://payport.novalnet.de/v2/transaction/details');
-            
-	    return $responseData;
-        }
-    }
-}
-
-    /**
-     * Process the Form payment
-     *
-     */
-    public function processPayment()
-    {
-        $requestData = $this->request->all();
-	    $this->getLogger(__METHOD__)->error('request controller', $requestData);
-        $notificationMessage = $this->paymentHelper->getNovalnetStatusText($requestData);
-        $birthday = sprintf('%4d-%02d-%02d',$requestData['nnBirthdayYear'],$requestData['nnBirthdayMonth'],$requestData['nnBirthdayDate']);
-        $serverRequestData = $this->paymentService->getRequestParameters($this->basketRepository->load(), $requestData['paymentKey']);
-	
-        if (empty($serverRequestData['data']['customer']['first_name']) && empty($serverRequestData['data']['customer']['last_name'])) {
-        $notificationMessage = $this->paymentHelper->getTranslatedText('nn_first_last_name_error');
+                $notificationMessage = $this->paymentHelper->getTranslatedText('checksumInvalid');
                 $this->paymentService->pushNotification($notificationMessage, 'error', 100);
                 return $this->response->redirectTo('checkout');
-        }
-	$paymentKey = explode('_', strtolower($requestData['paymentKey']));    
-	if (!empty($paymentKey[0].$paymentKey[1].'SelectedToken') && empty('newForm')) {
-                $paymentRequestParameters['transaction']['payment_data']['token'] = $paymentKey[0].$paymentKey[1].'SelectedToken';
-	} 
-	    
-	if($this->config->get('Novalnet.'. strtolower($requestData['paymentKey']) .'_shopping_type') == true) {
-			  $serverRequestData['data']['transaction']['create_token'] = 1;  
-		     }
-        
-        if($requestData['paymentKey'] == 'NOVALNET_CC') {
-            $serverRequestData['data']['transaction']['payment_data']['pan_hash'] = $requestData['nnCcPanHash'];
-            $serverRequestData['data']['transaction']['payment_data']['unique_id'] = $requestData['nnCcUniqueId'];
-            if($this->config->get('Novalnet.novalnet_cc_3d') == 'true' || $this->config->get('Novalnet.novalnet_cc_3d_fraudcheck') == 'true' )
-            {
-                $this->sessionStorage->getPlugin()->setValue('nnPaymentData', $serverRequestData['data']);
-                $this->sessionStorage->getPlugin()->setValue('nnPaymentUrl',$serverRequestData['url']);
-                $this->paymentService->pushNotification($notificationMessage, 'success', 100);
-                return $this->response->redirectTo('place-order');
+            } else {
+                $data = [];
+                $data['transaction']['tid'] = $response['tid'];
+                $responseData = $this->paymentHelper->executeCurl(json_encode($data), NovalnetConstants::TX_DETAILS_UPDATE_URL);
+                return $responseData;
             }
-        } elseif ( $requestData['paymentKey'] == 'NOVALNET_SEPA' ) {
-		
-		   if (empty($paymentKey[0].$paymentKey[1].'SelectedToken')) {
-    
-			    $serverRequestData['data']['transaction']['payment_data']['bank_account_holder'] = $serverRequestData['data']['customer']['first_name'] . ' ' . $serverRequestData['data']['customer']['last_name'];
-			    $serverRequestData['data']['transaction']['payment_data']['iban'] = $requestData['nnSepaIban'];   
-		   }  
-            } elseif ($requestData['paymentKey'] == 'NOVALNET_INSTALMENT_INVOICE' ) {
-		$serverRequestData['data']['transaction']['payment_type'] = 'INSTALMENT_INVOICE';
-		 $serverRequestData['data']['instalment']['interval'] = '1m';
-		 $serverRequestData['data']['instalment']['cycles'] = $requestData['nnInstalmentCycle'];
-		$serverRequestData['data']['customer']['birth_date']   =  $birthday;
-	}
-	    $this->getLogger(__METHOD__)->error('request controller', $serverRequestData);
-        $this->sessionStorage->getPlugin()->setValue('nnPaymentData', $serverRequestData);  
-        return $this->response->redirectTo('place-order');
+        }
     }
 
     /**
-     * Process the redirect payment
+     * Process the form payments - Credit card, Direct Debit SEPA and Instalment Invoice
      *
      */
-    public function redirectPayment()
+    public function processPayment() 
     {
-        $paymentRequestData = $this->sessionStorage->getPlugin()->getValue('nnPaymentData');
-        $orderNo = $this->sessionStorage->getPlugin()->getValue('nnOrderNo');
-        $paymentRequestData['order_no'] = $orderNo;
-        $paymentUrl = $this->sessionStorage->getPlugin()->getValue('nnPaymentUrl');
-
-        return $this->twig->render('Novalnet::NovalnetPaymentRedirectForm', [
-									   'formData'     => $paymentRequestData,
-									   'nnPaymentUrl' => $paymentUrl
-                                   ]);
+        $requestData = $this->request->all();
+        $birthday = sprintf('%4d-%02d-%02d',$requestData['nnBirthdayYear'],$requestData['nnBirthdayMonth'],$requestData['nnBirthdayDate']);
+        $paymentRequestParameters = $this->paymentService->getPaymentRequestParameters($this->basketRepository->load(), $requestData['paymentKey']);
+        if (empty($paymentRequestParameters['data']['customer']['first_name']) && empty($paymentRequestParameters['data']['customer']['last_name'])) {
+            $notificationMessage = $this->paymentHelper->getTranslatedText('firstLastNameError');
+            $this->paymentService->pushNotification($notificationMessage, 'error', 100);
+            return $this->response->redirectTo('checkout');
+        }
+        $paymentKey = explode('_', strtolower($requestData['paymentKey']));    
+        if (!empty($paymentKey[0].$paymentKey[1].'SelectedToken') && empty('newForm')) {
+            $paymentRequestParameters['transaction']['payment_data']['token'] = $paymentKey[0].$paymentKey[1].'SelectedToken';
+        } else {
+            // Common for one-click-shopping supported payments
+            if ($this->config->get('Novalnet.'. strtolower($requestData['paymentKey']) .'_shopping_type') == true) {
+              $paymentRequestParameters['data']['transaction']['create_token'] = 1;  
+            }
+            // Build request params for Credit card
+            if($requestData['paymentKey'] == 'NOVALNET_CC') {
+                $paymentRequestParameters['data']['transaction']['payment_data']['pan_hash'] = $requestData['nnCcPanHash'];
+                $paymentRequestParameters['data']['transaction']['payment_data']['unique_id'] = $requestData['nnCcUniqueId'];
+            } elseif ($requestData['paymentKey'] == 'NOVALNET_SEPA') { // Build request params for Direct Debit SEPA
+                $paymentRequestParameters['data']['transaction']['payment_data']['bank_account_holder'] = $paymentRequestParameters['data']['customer']['first_name'] . ' ' . $paymentRequestParameters['data']['customer']['last_name'];
+                $paymentRequestParameters['data']['transaction']['payment_data']['iban'] = $requestData['nnSepaIban']; 
+            } elseif ($requestData['paymentKey'] == 'NOVALNET_INSTALMENT_INVOICE') { // Build request params for Instalment Invoice
+                $paymentRequestParameters['data']['instalment']['interval'] = '1m';
+                $paymentRequestParameters['data']['instalment']['cycles'] = $requestData['nnInstalmentCycle'];
+                $paymentRequestParameters['data']['customer']['birth_date']   =  $birthday;
+            }
+        } 
+        $this->sessionStorage->getPlugin()->setValue('nnPaymentData', $paymentRequestParameters);
+        return $this->response->redirectTo('place-order');
     }
     
-	public function removeCard()
-	{
-	    $requestData = $this->request->all();
-	     $this->getLogger(__METHOD__)->error('reve controller', $requestData);
-	      $this->transaction->removeCardDetails('saveOneTimeToken', $requestData);
-	}
+    /**
+     * Remove the saved payment details from the database
+     *
+     */
+    public function removePaymentDetails() 
+    {
+        $requestData = $this->request->all();
+        $this->transaction->removeSavedPaymentDetails('saveOneTimeToken', $requestData);
+    }
 }
